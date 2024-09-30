92bbd7c0f23fe20c6732564db58089f0fa47048a
hachimabdel <abdelhachim@icloud.com>
2024-09-30 20:15:28 +0200

    abde

--- Pruebas/src/paquetePrueba/Main.java
+++ Pruebas/src/paquetePrueba/Main.java
@@ -1,9 +1,10 @@
-package clasefile;
+package paquetePrueba;
 
-public class FlujoCaracteres2 {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Me llamo Abdel");
 
 	}
 
